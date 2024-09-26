bb7b7004eb49430ab75c78838f6dfceb10ad003d
Juliano Silva <julianoqm@gmail.com>
2024-09-26 11:17:26 -0300

    correção de identação na atividade 6

--- tipos-variaveis/src/edu/respostas/tema3/Tema3Atv6.java
+++ tipos-variaveis/src/edu/respostas/tema3/Tema3Atv6.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
+
 public class Tema3Atv6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("Digite um texto:");
         String texto = scanner.nextLine();
-        
-  
-            char primeiro = texto.charAt(0);
-            char ultimo = texto.charAt(texto.length() - 1);
-            
-            System.out.println("Primeiro caractere: " + primeiro);
-            System.out.println("Último caractere: " + ultimo);
-  
-        
+
+        char primeiro = texto.charAt(0);
+        char ultimo = texto.charAt(texto.length() - 1);
+
+        System.out.println("Primeiro caractere: " + primeiro);
+        System.out.println("Último caractere: " + ultimo);
+
         scanner.close();
     }
 }

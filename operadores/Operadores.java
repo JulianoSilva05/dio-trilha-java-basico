294f9b9a98026031644f41bf7c1519991a04b21b
Juliano Silva <julianoqm@gmail.com>
2024-09-27 10:06:32 -0300

    operadores e concatenação

--- operadores/Operadores.java
+++ operadores/Operadores.java
@@ -3,7 +3,7 @@ import java.util.Date;
 
 public class Operadores {
     public static void main(String[] args) {
-        //Variaveis
+        // Variaveis
         String nome = "Juliano";
         int idade = 21;
         double peso = 68.5;
@@ -11,17 +11,31 @@ public class Operadores {
         boolean estudante = true;
         Date dataNascimento = new Date();
 
-        //Operadores
+        // Operadores
         double soma = 10.5 + 15.7;
         int subtracao = 113 - 25;
         int multiplicacao = 20 * 7;
-        int divisao = 15/3;
-        int modulo = 18%3;
-        double resultado = (10 * 7) + (20/4);
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20 / 4);
 
-        //Concatenação
+        // Concatenação
         String nomeCompleto = "Juliano" + "Silva";
-        
+        System.err.println(nomeCompleto);
 
+        // qual o resultado da expressão abaixo
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + 1  + "1" + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1   + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+ (1 + 1 + 1);
+        System.out.println(concatenacao);
     }
 }

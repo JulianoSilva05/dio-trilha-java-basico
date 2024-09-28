5303ab72882d8443e2379dc2ce648ee62bde9c41
Juliano Silva <julianoqm@gmail.com>
2024-09-28 17:16:02 -0300

    Operadores

---
+++ operadores/OperadorQuatro.java
@@ -0,0 +1,10 @@
+public class OperadorQuatro {
+    public static void main(String[] args) {
+        int numero = 5;
+        //x repetições
+        System.out.println(numero++);
+        numero = numero + 1;
+
+        System.out.println(numero);
+    }
+}
